c6b40c6dda7ba2d0ec3087943d7184f595db053c
hannahmerl <91705952+hannahmerl@users.noreply.github.com>
2022-12-06 13:43:27 +0800

    Sign in page - js

---
+++ src/signInPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import SignInPage from "./components/SignInPage/signInPage";
+
+const SIPage = () => {
+    return (
+        <div>
+            <SignInPage/>
+        </div>
+    )
+}
+export default SIPage;
