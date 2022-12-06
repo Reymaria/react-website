cd3cdc8924c252f69ff7caeb334c57a3b8ef37be
Florence Visperas <91741766+florencevisperas@users.noreply.github.com>
2022-12-06 13:19:31 +0800

    Add files via upload

---
+++ src/signUpPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import SignUpPage from "./components/SignUpPage/signUpPage";
+const SUPage = () => {
+    return (
+        <div>
+            <SignUpPage/>
+        </div>
+    )
+}
+export default SUPage;
